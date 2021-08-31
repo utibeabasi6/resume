612c632da4c2070c5dc03ac8e85b1022ba727de4
M <mikhaelnwankwo@gmail.com>
2021-08-31 10:36:27 +0100

    Update Header.jsx

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -10,9 +10,9 @@ const Header = ({ toggleDrawer }) => {
     <div style={{ backgroundColor: '#f8e4d4', borderRadius: '0 0 24px 0', height: '296px' }} className={`flex flex-col justify-between lg:justify-end w-full`}>
       <Container className={`w-full flex justify-between lg:justify-start`}>
         <div className={`flex items-center space-x-4`}>
-          {data.logos.map((logo, idx) => {
-            return <img className={`rounded-full`} width={50} key={idx} src={logo} alt='logo' />;
-          })}
+//           {data.logos.map((logo, idx) => {
+//             return <img className={`rounded-full`} width={50} key={idx} src={logo} alt='logo' />;
+//           })}
         </div>
         <data.MenuIcon className={`lg:hidden  text-gray-800 cursor-pointer`} onClick={toggleDrawer} size={24} />
       </Container>
